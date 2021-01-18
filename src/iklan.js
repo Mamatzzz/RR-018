d1e421c0965b4d52dc24fed309c829b422e1942f
M.Rizki R <74702393+rizkiramadhan4617@users.noreply.github.com>
2021-01-18 20:52:36 +0700

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,20 @@
+const iklan = (prefix) => { 
+	return `  Hai stah👋
+*||────────────────────||* 
+  *メメメ[INFORMATION]メメメ*
+*||────────────────────||*
+*-Mau membuat bot seperti ini?*
+*-Mau jadi user prem & membuka fitur prem?*
+─────────────────────
+*-Jika ingin membuat bot seperti ini silahkan berdonasi 10k info lanjut ketik ${prefix} donasi*:) 
+*-Jika ingin menjadi user prem silahkan berdonasi 5k info lanjut ketik ${prefix} donasi(:*
+*_Spesial thanks to_:*
+_-Arif👑_
+_-RR.018👑_
+_-Aris👑_
+_-Iksan👑_
+_-mhankbarbar👑_
+_-arugaZ👑_
+`
+}
+exports.iklan = iklan
